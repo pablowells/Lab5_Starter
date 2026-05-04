ad71055f681d72a4ac302cd431d4212bf6310203
pablowells <pablobwells7@gmail.com>
2026-05-04 13:59:00 -0700

    Complete expose

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lab 5 - Starter
+Pablo Wells (no partners)
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,44 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const img = document.querySelector('img[alt="No image selected"]');
+    const sound = document.querySelector('audio');
+    const selectSound = document.querySelector('select');
+    
+    selectSound.addEventListener("change", (event) => {
+      img.src = 'assets/images/' + event.target.value + '.svg';
+      sound.src = 'assets/audio/' + event.target.value + '.mp3';
+    });
+
+    const range = document.querySelector('[type="range"]');
+    const volImage = document.querySelector('img[alt="Volume level 2"]')
+    range.addEventListener("change", (event) => {
+      sound.volume = +(event.target.value) / 100;
+      console.log(sound.volume);
+      const height = volImage.clientHeight;
+      const width = volImage.clientWidth;
+      if (+(event.target.value)  == 0) {
+        volImage.src = "assets/icons/volume-level-0.svg";
+      }
+      else if (1 <= +(event.target.value) &&  +(event.target.value)<= 32) {
+        volImage.src = "assets/icons/volume-level-1.svg";
+      }
+      else if  (33 <= +(event.target.value) && +(event.target.value) <= 66) {
+        volImage.src = "assets/icons/volume-level-2.svg";
+      }
+      else if (67 <= +(event.target.value)) {
+        volImage.src = "assets/icons/volume-level-3.svg";
+      }
+    });
+    const button = document.querySelector('button');
+    button.addEventListener("click", (event) => {
+      if(selectSound.value == 'party-horn') {
+        const jsConfetti = new JSConfetti();
+        sound.play();
+        jsConfetti.addConfetti();
+      } else if (selectSound.value){
+        sound.play();
+      }
+    })
 }
